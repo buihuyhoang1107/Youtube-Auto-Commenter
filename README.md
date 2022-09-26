e82bf90d584925dbfc0aeccbb708e0b6120752b9
buihuyhoang <bhhoang@troodonlabs.com>
2022-09-26 09:22:05 +0700

    feat: auto comment google

---
+++ README.md
@@ -0,0 +1,120 @@
+
+
+  <p align="center">
+  <image src="https://hamptonlittleleague.files.wordpress.com/2022/03/youtube-logo.wine_.png" align="center"  width="400"/>
+<h1 align="center"> Youtube-Auto-Commenter </h1>
+Youtube Bot Auto Commenter Using Puppeter and NodeJS
+    </p>
+    
+## Quick Links
+
+* [Demo](https://youtu.be/KX2L68_cXeU)
+* [Installation](#run--installation)
+* [Feature](#feature)
+* [Configuration](#configuration)
+
+
+
+## Demo
+ <a href="https://youtu.be/KX2L68_cXeU">Video demo</a>
+
+```bash
+$ see this DEMO in here > https://youtu.be/KX2L68_cXeU
+
+```
+
+## Run / Installation
+How to run ?
+
+```bash
+> git clone https://github.com/kugayamashusei/Youtube-Auto-Commenter.git
+> cd Youtube-Auto-Commenter
+> npm install
+> node main.js
+```
+
+dont forget to change the google username and password in ` config.js`
+
+```javascript
+module.exports = {
+  keywords: "keywords",
+  usernamegoogle: "username",
+  passwordgoogle: "pass",
+};
+
+```
+## Feature
+
+ Feature  | Status |
+| -------------:|------------- |
+| Random Copy Comment | Ok|
+| Comment On Trending | Ok |
+| Comment based on Keyword | Ok |
+| Multi Account |  Ok|
+
+## Requirements
+
+ - Windows / Linux
+ - Google Chrome
+ - a Youtube account 
+
+## Running on android 
+
+yes of course the bot is still works on android
+
+but you need ubuntu installed on your android , use Termux for it!
+and install ubuntu desktop with Andronix 
+i prefer ubuntu 20 , cuz it's more stable than 18 
+
+command for installing inside the ubuntu 
+
+copy and paste one by one , don't be lazy dude 🤨
+
+```bash
+> apt instal
+> apt update
+> apt upgrade 
+> apt install chromium-browser
+> apt install npm
+> npm install -g n
+> n install lts
+> hash -r 
+> apt install curl
+> curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+> echo "deb https://dl.yarnpkg.com/debian/ stable main" |  tee /etc/apt/sources.list.d/yarn.list
+> apt install yarn 
+```
+
+go to directory of this project , then install the dependency
+
+```bash
+yarn
+```
+
+
+also you need to change `executableFilePath` in `main.js` 
+you can set to /usr/bin/chromium
+
+## Configuration 
+
+ Config  | Info |
+| -------------:|------------- |
+| keyword | fill the keyword , for searching videos of course|
+| comments | list of the comment |
+| usernamegoogle | your username google |
+| password |  your google password|
+| delaycomment |  delay commenting / seconds |
+| trending | Comment based on trending videos on your location|
+| multiaccount |  Multiple account , loop based and not multi threaded |
+
+
+## Donate
+
+???
+
+
+
+
+
+           
+
---
+++ multi.js
@@ -0,0 +1,211 @@
+const puppeteer = require('puppeteer-extra');
+const StealthPlugin = require('puppeteer-extra-plugin-stealth')();
+const config = require('./accs');
+const {
+  chromepath,
+  subsribe,
+  copycommnet,
+  manualComment,
+  autoscroll,
+} = require('./modules');
+const cliSpinners = require('cli-spinners');
+const Spinners = require('spinnies');
+['chrome.runtime', 'navigator.languages'].forEach((a) =>
+  StealthPlugin.enabledEvasions.delete(a)
+);
+
+const spinners = new Spinners(cliSpinners.star.frames, {
+  text: 'Loading',
+  stream: process.stdout,
+  onTick: function (frame, index) {
+    process.stdout.write(frame);
+  },
+});
+
+const wait = (seconds) =>
+  new Promise((resolve) => setTimeout(() => resolve(true), seconds * 1000));
+
+puppeteer.use(StealthPlugin);
+
+let paths = process.cwd() + '/ublock';
+
+function browserconfig(paths, config) {
+  return {
+    defaultViewport: null,
+    // devtools: true,
+    headless: false,
+    // executablePath: chromepath,
+    args: [
+      '--log-level=3', // fatal only
+
+      '--no-default-browser-check',
+      '--disable-infobars',
+      '--disable-web-security',
+      '--disable-site-isolation-trials',
+      '--no-experiments',
+      '--ignore-gpu-blacklist',
+      '--ignore-certificate-errors',
+      '--ignore-certificate-errors-spki-list',
+      '--mute-audio',
+      '--disable-extensions',
+      '--no-sandbox',
+
+      '--no-first-run',
+      '--no-zygote',
+      `--disable-extensions-except=${paths}`,
+      `--load-extension=${paths}`,
+    ],
+    userDataDir: config.userdatadir,
+  };
+}
+
+async function startApp(config, browserconfig) {
+  var keyword = config.keywords;
+
+  const browser = await puppeteer.launch(browserconfig);
+  const page = await browser.newPage();
+  await page.setViewport({ width: 1366, height: 768 });
+  await page.evaluateOnNewDocument(() => {
+    delete navigator.__proto__.webdriver;
+  });
+
+  await page.setUserAgent(
+    'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
+  );
+
+  await page.goto(
+    'https://accounts.google.com/signin/v2/identifier?service=youtube'
+  );
+  spinners.add('user', { text: 'Login..', color: 'green' });
+
+  await page.waitForTimeout(2000);
+
+  try {
+    let checklogin = await page.$(
+      '#yDmH0d > c-wiz > div > div:nth-child(2) > div > c-wiz > c-wiz > div > div.s7iwrf.gMPiLc.Kdcijb > div > div > header > h1'
+    );
+    await page.evaluate((el) => el.textContent, checklogin);
+    spinners.succeed('user', {
+      text: 'You already logged in..',
+      color: 'blue',
+    });
+  } catch {
+    await page.waitForSelector('#identifierId');
+    await page.type('#identifierId', config.usernamegoogle, { delay: 400 });
+    await page.waitForTimeout(1000);
+    await page.keyboard.press('Enter');
+    await page.waitForTimeout(5000);
+    await page.type('input', config.passwordgoogle, { delay: 400 });
+    await page.keyboard.press('Enter');
+    await page.waitForTimeout(10000);
+  }
+  console.log('=========== Start Commenting ==============');
+
+  try {
+    await subsribe.subscribeChannel(page);
+  } catch (error) {
+    console.error(error);
+  }
+
+  spinners.add('first-spinner', {
+    text: 'Searching for videos..',
+    color: 'blue',
+  });
+  for (let i = 0; i < keyword.length; i++) {
+    if (config.trending == true) {
+      await page.goto('https://www.youtube.com/feed/trending');
+      await autoscroll._autoScroll(page);
+    } else {
+      await page.goto(
+        'https://www.youtube.com/results?search_query=' +
+          keyword[i] +
+          '&sp=EgQQARgD'
+      );
+      await autoscroll._autoScroll(page);
+    }
+
+    await page.waitForTimeout(3000);
+    spinners.succeed('first-spinner', { text: 'done..', color: 'blue' });
+    await page.waitForTimeout(7000);
+    spinners.add('hasil', { text: 'Collecting videos..', color: 'yellow' });
+    const linked = await page.$$('#video-title');
+
+    var link = linked.filter(function (el) {
+      return el != null;
+    });
+    spinners.succeed('hasil', {
+      text: 'FOUND ' + link.length + 'LINKS',
+      color: 'yellow',
+    });
+
+    for (i in link) {
+      spinners.add('comment', {
+        text: 'Now commenting in the video..',
+        color: 'blue',
+      });
+      const tweet = await (await link[i].getProperty('href')).jsonValue();
+
+      const pages = await browser.newPage();
+      await pages.setViewport({ width: 1366, height: 768 });
+      await pages.setUserAgent(
+        'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36'
+      );
+      try {
+        await pages.goto(tweet);
+        await pages.bringToFront();
+        await page.waitForTimeout(4000);
+        await pages.evaluate(() => {
+          window.scrollBy(0, 650);
+        });
+
+        try {
+          await pages.waitForSelector('#message > span', { timeout: 4000 });
+          console.log("Can't Comment");
+          await pages.close();
+        } catch {
+          await pages.waitForSelector('#simplebox-placeholder', {
+            timeout: 4000,
+          });
+
+          await pages.evaluate(() => {
+            document.querySelector('#simplebox-placeholder').click();
+          });
+
+          spinners.update('comment', {
+            text: 'So.. we need collecting those comment , so we can copy that ',
+            color: 'blue',
+          });
+
+          if (config.copycomment == true) {
+            await copycommnet.copyComment(pages, spinners);
+          } else {
+            await manualComment.manualComment(pages, spinners, config);
+          }
+          await page.waitForTimeout(4000);
+          await pages.close();
+          spinners.succeed('comment', {
+            text: 'Success commenting',
+            color: 'blue',
+          });
+        }
+      } catch (e) {
+        await pages.close();
+        console.log(
+          'Something Wrong maybe this is Short videos , live stream , or broken error : ' +
+            e
+        );
+      }
+      await wait(10);
+    }
+
+    spinners.add('delay', {
+      text: 'Wait .. we were delaying for ' + config.delay + ' menit',
+      color: 'blue',
+    });
+  }
+  console.log('DONE! HAVE A NICE DAY');
+}
+for (let i = 0; i < config.data.length; i++) {
+  startApp(config.data[i], browserconfig(paths, config.data[i]));
+  wait(10000);
+}
